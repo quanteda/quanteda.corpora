8c5aa074d012dd32495b22eb1b8c0f1fa409fe2e
Ken Benoit <kbenoit@lse.ac.uk>
2014-09-15 09:09:22 -0400

    moved construction code out of R directory, tried to export to NAMESPACE - where are the man pages??

--- R/dataDocs.R
+++ R/dataDocs.R
@@ -2,12 +2,14 @@
 #' @title Corpus of US presidential inaugaration speeches
 #' @description Corpus of US presidential inaugaration speeches
 #' @docType data
+#' @export
 NULL
 
 #' @name inaugTexts
 #' @title Texts of US presidential inaugaration speeches
 #' @description Texts of US presidential inaugaration speeches
 #' @docType data
+#' @export
 NULL
 
 #' @name amicusCorpus
