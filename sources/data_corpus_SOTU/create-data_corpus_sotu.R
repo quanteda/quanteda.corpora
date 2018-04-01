fc61cdd572aa4f62ee68b2055b16f50453d64e9a
Kenneth Benoit <kbenoit@lse.ac.uk>
2018-04-01 09:52:22 +0100

    Update SOTU sources and data object

---
+++ sources/data_corpus_SOTU/create-data_corpus_sotu.R
@@ -0,0 +1,26 @@
+## make the SOTU corpus
+
+library("quanteda")
+
+SOTUdocvars <- read.csv("sources/data_corpus_SOTU/SOTU_metadata.csv", stringsAsFactors = FALSE)
+SOTUdocvars$Date <- as.Date(SOTUdocvars$Date, "%B %d, %Y")
+SOTUdocvars$delivery <- as.factor(SOTUdocvars$delivery)
+SOTUdocvars$type <- as.factor(SOTUdocvars$type)
+SOTUdocvars$party <- as.factor(SOTUdocvars$party)
+SOTUdocvars$nwords <- NULL
+
+
+library("readtext")
+SOTUCorpus <- corpus(readtext("sources/data_corpus_SOTU/su*.txt"),
+                     metacorpus = list(source = "http://www.presidency.ucsb.edu/sou.php"))
+docvars(SOTUCorpus) <- c(docvars(SOTUCorpus), SOTUdocvars)
+docnames(SOTUCorpus) <- paste(docvars(SOTUCorpus, "President"), 
+                              gsub("su|\\.txt", "", docvars(SOTUCorpus, "filename")), 
+                              sep = "-")
+docvars(SOTUCorpus, setdiff(names(docvars(SOTUCorpus)), 
+                            names(docvars(quanteda.corpora::data_corpus_sotu)))) <- NULL
+
+data_corpus_sotu <- SOTUCorpus
+
+devtools::use_data(data_corpus_sotu, overwrite = TRUE)
+
