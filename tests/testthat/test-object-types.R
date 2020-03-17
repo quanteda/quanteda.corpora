48f5d7568bbacdaff6351494ec2da51aac3c04ba
kbenoit <kbenoit@lse.ac.uk>
2020-03-17 15:24:23 +1100

    remove test for movies

--- tests/testthat/test-object-types.R
+++ tests/testthat/test-object-types.R
@@ -4,7 +4,6 @@ test_that("objects are correct type", {
     expect_is(data_corpus_amicus, "corpus")
     expect_is(data_corpus_immigrationnews, "corpus")
     expect_is(data_corpus_irishbudgets, "corpus")
-    expect_is(data_corpus_movies, "corpus")
     expect_is(data_corpus_sotu, "corpus")
     expect_is(data_corpus_ukmanifestos, "corpus")
     expect_is(data_corpus_ungd2017, "corpus")
