ea65060a689974cb47bcd5be541d8414fd8c6993
Kenneth Benoit <kbenoit@lse.ac.uk>
2025-09-25 18:23:07 +0800

    Update SOTU speeches

--- sources/sotu/create-data_corpus_sotu.R
+++ sources/sotu/create-data_corpus_sotu.R
@@ -2,23 +2,31 @@
 
 library("quanteda")
 
-SOTUdocvars <- read.csv("sources/data_corpus_SOTU/SOTU_metadata.csv", stringsAsFactors = FALSE)
+SOTUdocvars <- read.csv("sources/sotu/SOTU_metadata.csv", stringsAsFactors = FALSE)
 SOTUdocvars$Date <- as.Date(SOTUdocvars$Date, "%B %d, %Y")
 SOTUdocvars$delivery <- as.factor(SOTUdocvars$delivery)
 SOTUdocvars$type <- as.factor(SOTUdocvars$type)
 SOTUdocvars$party <- as.factor(SOTUdocvars$party)
 SOTUdocvars$nwords <- NULL
 
-
 library("readtext")
-SOTUCorpus <- corpus(readtext("sources/data_corpus_SOTU/su*.txt"),
-                     metacorpus = list(source = "http://www.presidency.ucsb.edu/sou.php"),
+SOTUCorpus <- corpus(readtext("sources/sotu/su*.txt"),
                      docid_field = "doc_id")
+
+meta(SOTUCorpus) <- list(
+    title = "U.S. State of the Union addresses from 1790 to present", 
+    description = "A corpus object of every US State-of-the-Union address, from 1790 to present. Where interjections or records of audience reaction occurred, such as \"(Applause.)\", these have been removed.  Includes first and last name, date, delivery type (written or spoken), the address type (SOTU or other), and the President's party.", 
+    source = "Gerhard Peters and John T. Woolley. The American Presidency Project.", 
+    keywords = c("political", "US politics", "United States", 
+                 "presidents", "presidency", "state of the union"), 
+    url = "https://www.presidency.ucsb.edu/documents/app-categories/spoken-addresses-and-remarks/presidential/state-the-union-addresses"
+)
+
 
 # check that docnames and filenames are the same
 stopifnot(all.equal(SOTUdocvars$filename, docnames(SOTUCorpus)))
 
-docvars(SOTUCorpus) <- c(docvars(SOTUCorpus), SOTUdocvars)
+docvars(SOTUCorpus) <- as.data.frame(c(docvars(SOTUCorpus), SOTUdocvars))
 docnames(SOTUCorpus) <- paste(docvars(SOTUCorpus, "President"), 
                               gsub("su|\\.txt", "", docvars(SOTUCorpus, "filename")), 
                               sep = "-")
